427536f1cf2c430fffe0be6cc3903c41b9b22fe3
Azula <sakthibala3107@gmail.com>
2026-03-15 21:16:28 +0530

    [upd] contact form

--- src/app/pages/contact-us-form/contact-us-form.component.ts
+++ src/app/pages/contact-us-form/contact-us-form.component.ts
@@ -1,15 +1,44 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+  FormGroup,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-contact-us-form',
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './contact-us-form.component.html',
-  styleUrl: './contact-us-form.component.scss'
+  styleUrl: './contact-us-form.component.scss',
 })
 export class ContactUsFormComponent {
   form: FormGroup;
 
+  weekendFilter = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    return day !== 0 && day !== 6;
+  };
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       name: ['', Validators.required],
@@ -18,7 +47,7 @@ export class ContactUsFormComponent {
       topic: [''],
       priority: ['medium'],
       subscribe: [false],
-      contactDate: [null]
+      contactDate: [null],
     });
   }
 
