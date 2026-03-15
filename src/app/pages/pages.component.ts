8b52aae89b5d6582dec78ef8d85c2eec30e1fbf5
Azula <sakthibala3107@gmail.com>
2026-03-15 13:05:24 +0530

    [upd]

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -16,6 +16,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import {
   MatButtonToggleGroup,
   MatButtonToggleModule,
@@ -35,6 +36,7 @@ import {
     MatToolbarModule,
     MatListModule,
     MatIconModule,
+    MatButtonModule,
   ],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss',
