53098512adaa6557751d7a38465ceebe33b8d8c5
Azula <sakthibala3107@gmail.com>
2026-03-15 22:42:02 +0530

    [upd] mastered snackbar

--- src/app/pages/user-profile-manager/edit-profile-dialog/edit-profile-dialog.component.ts
+++ src/app/pages/user-profile-manager/edit-profile-dialog/edit-profile-dialog.component.ts
@@ -1,29 +1,52 @@
-import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, Inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
-
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { DialogRef } from '@angular/cdk/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-edit-profile-dialog',
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatButton,
+  ],
   templateUrl: './edit-profile-dialog.component.html',
-  styleUrl: './edit-profile-dialog.component.scss'
+  styleUrl: './edit-profile-dialog.component.scss',
 })
 export class EditProfileDialogComponent {
   form: FormGroup;
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: { name: string; bio: string },
+    public dialog: MatDialogRef<EditProfileDialogComponent>,
   ) {
-
     this.form = this.fb.group({
-      name: ['', Validators.required],
-      bio: ['']
+      name: [data?.name, Validators.required],
+      bio: [data?.bio],
     });
   }
 
   save() {
     if (this.form.valid) {
-      console.log(this.form.value)
+      console.log(this.form.value);
     }
+    this.dialog.close();
   }
 }
--- src/app/pages/user-profile-manager/user-profile-manager.component.ts
+++ src/app/pages/user-profile-manager/user-profile-manager.component.ts
@@ -1,20 +1,39 @@
 import { Component, inject } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
+import { EditProfileDialogComponent } from './edit-profile-dialog/edit-profile-dialog.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DialogRef } from '@angular/cdk/dialog';
 @Component({
   selector: 'app-user-profile-manager',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './user-profile-manager.component.html',
-  styleUrl: './user-profile-manager.component.scss'
+  styleUrl: './user-profile-manager.component.scss',
 })
 export class UserProfileManagerComponent {
-
   user = {
     name: 'John Doe',
-    bio: 'Angular developer & UI designer.'
+    bio: 'Angular developer & UI designer.',
   };
 
-  openDialog(): void {
+  readonly dialog = inject(MatDialog);
+  readonly snackBar = inject(MatSnackBar);
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(EditProfileDialogComponent, {
+      data: this.user,
+      height: '400px',
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe((res) => {
+      if (res) {
+        this.user = res;
+        this.snackBar.open('Profile Updated', 'Close', { duration: 3000 });
+      }
+    });
   }
 
+  //
 }
