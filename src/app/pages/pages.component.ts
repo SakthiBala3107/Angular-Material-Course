e406b7fbb94280c996a79c7c23c14668e4c7c28b
Azula <sakthibala3107@gmail.com>
2026-03-15 18:08:23 +0530

    [upd] mastered  breakpoint observer

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import {
   ActivatedRoute,
@@ -7,7 +7,7 @@ import {
   RouterLink,
   RouterOutlet,
 } from '@angular/router';
-import { filter } from 'rxjs';
+import { filter, map } from 'rxjs';
 import { ThemeService } from '../services/theme.service';
 import { FormsModule } from '@angular/forms';
 import { LoaderService } from '../services/loader.service';
@@ -25,6 +25,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pages',
@@ -45,6 +47,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
     MatProgressBarModule,
     MatSlideToggleModule,
     MatGridListModule,
+    CommonModule,
   ],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss',
@@ -62,6 +65,20 @@ export class PagesComponent {
     public themeService: ThemeService,
     public loaderService: LoaderService,
   ) {
+    // this.breakpointObserver
+    //   .observe([Breakpoints.Small, Breakpoints.XSmall])
+    //   .subscribe((state) => {
+    //     console.log('RAW STATE:', state);
+    //   });
+
+    // this.breakpointObserver
+    //   .observe([Breakpoints.Small, Breakpoints.XSmall])
+    //   .pipe(map((state) => (state.matches ? 1 : 2)))
+    //   .subscribe((cols) => {
+    //     console.log('TRANSFORMED VALUE:', cols);
+    //   });
+
+    //
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe(() => {
@@ -74,6 +91,12 @@ export class PagesComponent {
     this.selectedColor = themeService.color();
     this.selectedMode = themeService.mode();
   }
+
+  private breakpointObserver = inject(BreakpointObserver);
+
+  readonly col$ = this.breakpointObserver
+    .observe([Breakpoints.Small, Breakpoints.XSmall, Breakpoints.Medium])
+    .pipe(map((state) => (state.matches ? 1 : 2)));
 
   getChild(route: ActivatedRoute): ActivatedRoute {
     if (route.firstChild) {
